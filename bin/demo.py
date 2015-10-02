7d0c53ac101074a9d45d5aa6ad8aa9debb519fdf
yibo <yiboyang@ymail.com>
2015-10-01 19:47:52 -0500

    Fix basically everything :)

--- bin/demo.py
+++ bin/demo.py
@@ -1,90 +1,28 @@
-# import random as r
-# lowerChars='abcdefghijklmnopqrstuvwxyz'
-# idx=list(range(26)) # indices into the lowerChars string; explicitly cast as list for it to work in python3
-# 
-# 
-# # given a character set and center letter, output all the possible solutions
-# # this is the brute-force approach; optimization later
-# def solutions(chars, center):
-#     assert center in chars
-#     sol=[]
-#     f=open("../lib/"+center+'.txt','r') # open the preprocessed file for searching
-#     for line in f:
-#         goodLine=True
-#         for ch in line[:-1]: # get rid of the newline at the end
-#             if ch not in chars: # if even one of them is not desired
-#                 goodLine=False
-#                 continue
-#         if (goodLine):
-#             sol.append(line[:-1]) # if every char from this line is good: solution
-#     f.close()
-#     return sol
-# 
-# 
-# print("--------Lexathon!!!!--------")
-# 
-# while (True):
-#     print("----------------------------")
-#     quit=input("enter 'q' to quit, or anything else to start a new round: ")
-#     if (quit=='q' or quit=='Q'):
-#         break;
-#     # randomly select 9 unique chars to play the game; 4th char is the center
-#     r.shuffle(idx)
-#     chars=[lowerChars[i] for i in idx[:9]]
-#     center=chars[4]
-#     # print the grid
-#     for j in range(9):
-#         print(chars[j]+" ", end="") # force print without newline in py3
-#         if (j%3==2):
-#             print()
-#     soln=solutions(chars, center)
-#     ans=[]
-#     while (True):
-#         print()
-#         entered=input("enter your word (4-9 char lower case, must use the letter at the centerof the grid): ")
-#         if (entered in soln and entered not in ans):
-#             print("Right on!")
-#             ans.append(entered)
-#         elif (entered in ans):
-#             print("You already found it!")
-#         else:
-#             print("Nope...")
-#         print("Words found so far: ",ans)
-#         print(str(len(ans))+" out of "+str(len(soln)))
-#         todo=input("enter 'S' for all solutions, 'D' to be done with this round, or anything else to keep playing: ")
-#         if (todo=='S'):
-#             print(soln)
-#         elif (todo=='D'):
-#             break
-# 
-# print("bye")
-
-
-
 # this is the version that allows duplicate letters in the grid (note that the preprocessed dictionaries should be updated accordingly)
+# make sure you're using python3
+# by Yibo Yang
 import random as r
 lowerChars='abcdefghijklmnopqrstuvwxyz'
 
 
-# given a character set and center letter, output all the possible solutions
+# given a character set and center letter, output all the possible solutions by examing the dictionary file corresponding to center character
 # this is probably not the most efficient approach; since we now allow repetitive letters (but they have to all come from the grid),
-# we'll have to also consider the number of occurences of each letter in an entered. The most straightforward solution is to use a list
-# of grid chars and remove letters from it when scanning entries letter by letter in dictionary; we skip that entered once a remove fails; 
-# otherwise we considers it a solution
-
+# we'll have to also consider the number of occurences of each letter in an entry. The most straightforward solution is to use the list
+# of grid chars and remove letters from it as we scan each entry letter by letter in dictionary; we skip that entry once a remove fails; 
+# otherwise we accept it as a solution
 def solutions(chars, center):
     assert center in chars
-    sol=[]
+    sol=[] # assuming the dictionary files are nice and contain no duplicates; otherwise we have to use a set
     f=open("../lib/"+center+'.txt','r') # open the preprocessed file for searching
     for line in f:
-        pool=chars[:] # make a deep copy; this could be inefficient; instead, use a boolean array for this operation (reset everytime)
+        pool=chars[:] # make a deep copy; this could be inefficient; instead, next time use a boolean array for this operation (reset everytime)
         goodLine=True
-        for ch in line[:-1]: # get rid of the newline at the end
+        for ch in line.rstrip(): # get rid of the newline at the end
             try:
                 pool.remove(ch) # throws exception when can't remove (removes one entered at a time)
             except ValueError: # if even one of them is not in our pool
                 goodLine=False
-                continue
+                break # in previous versions this was 'continue'--wrong wrong wrong!
         if (goodLine):
             sol.append(line[:-1]) # every char from this line is good? : solution
     f.close()
@@ -93,14 +31,14 @@ def solutions(chars, center):
 # print list of 9 chars in a grid
 def printGrid(chars): 
     assert len(chars)==9
-    print("---------")
+    print("------")
     for j in range(9):
         print(chars[j]+" ", end="") # force print without newline in py3
         if (j%3==2):
             print()
-    print("---------")
+    print("------")
 
-
+# main
 def game():
     print("--------Lexathon!!!!--------")
     while (True):
@@ -111,7 +49,6 @@ def game():
         # randomly select 9 unique chars to play the game; 4th char is the center
         idx=[r.randrange(26) for _ in range(9)] # generate 9 non-unique random numbers in [0, 25]
         chars=[lowerChars[i] for i in idx[:9]] # index into lowerChars to get random letters list
-        # charSet=set(chars) # convert to the set of unique chars generated for each round
         center=chars[4] # get char at the center of the grid that determines solutions
         
         soln=solutions(chars, center)
@@ -126,6 +63,9 @@ def game():
                 continue
             elif (entered=='D'):
                 break
+            elif (len(entered)<4 or len(entered)>9): # minimal input validation :)
+                print("Make sure your entry has 4-9 chars!")
+                continue
             elif (entered in soln and entered not in ans):
                 print("Right on!")
                 ans.append(entered)
@@ -137,10 +77,6 @@ def game():
             print(str(len(ans))+" out of "+str(len(soln)))
     
     print("bye")
-
-
-
-
 
 
 if __name__ == "__main__":
